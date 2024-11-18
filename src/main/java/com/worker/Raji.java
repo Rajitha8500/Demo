e1aaf2abb9d78a05b67c629607a420dbe703f65f
N Rajhitha <raji1234@gmail.com>
2024-11-18 15:48:40 +0530

    Erro 56789 fixed by gaurav kumar

---
+++ src/main/java/com/worker/Raji.java
@@ -0,0 +1,7 @@
+package com.worker;
+
+public class Raji {
+    public static void main(String[] args) {
+        System.out.println("hello meneger how it is working");
+    }
+}
---
+++ src/main/java/com/worker/gaurav/dfghjkl.java
@@ -0,0 +1,7 @@
+package com.worker.gaurav;
+
+public class dfghjkl {
+    public static void main(String[] args) {
+        System.out.println("hello gaurav");
+    }
+}
